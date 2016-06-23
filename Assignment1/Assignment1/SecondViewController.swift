c72edadab0f490a22acb8fd9d168da7fc7220aee
taby1 <pyoon99@gmail.com>
2016-06-22 23:45:39 -0400

    Assignment 1 Started
    The beginnings begin

---
+++ Assignment1/Assignment1/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Assignment1
+//
+//  Created by Patrick on 6/22/16.
+//  Copyright © 2016 Taby Corp. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
