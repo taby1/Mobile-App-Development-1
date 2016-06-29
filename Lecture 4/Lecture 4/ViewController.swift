1fee6125eb042f68f8ef885e1014d1703719a195
taby1 <pyoon99@gmail.com>
2016-06-29 17:47:43 -0400

    Assignment 2 Started
    I have started, and renamed ViewController.swift to MainController.swift

---
+++ Lecture 4/Lecture 4/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Lecture 4
+//
+//  Created by Patrick on 6/29/16.
+//  Copyright © 2016 Taby Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func thgansdlkf(sender: AnyObject) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
