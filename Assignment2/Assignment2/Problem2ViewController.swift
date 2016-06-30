2e906b17eac276cac7219bcc1348f0988d00f820
taby1 <pyoon99@gmail.com>
2016-06-30 16:50:15 -0400

    Progressing
    Addes .swift files for each view controller and added code to add the
    title; linked and layed out buttons

---
+++ Assignment2/Assignment2/Problem2ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by Patrick on 6/29/16.
+//  Copyright © 2016 Taby Corp. All rights reserved.
+//
+
+import UIKit
+
+class Problem2ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Problem 2"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ Assignment2/Assignment2/Problem4ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by Patrick on 6/29/16.
+//  Copyright © 2016 Taby Corp. All rights reserved.
+//
+
+import UIKit
+
+class Problem4ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Problem 4"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
