04931862365752d8604f8758b9dac9d7b9cc13a1
taby1 <pyoon99@gmail.com>
2016-07-09 23:17:01 -0400

    Done! Everything works as it must.
    Yay! Yay!

--- Assignment3/Assignment3/Engine.swift
+++ Assignment3/Assignment3/Engine.swift
@@ -55,7 +55,11 @@ enum CellState:String{
             initCells()
         }
     }
-    var grid:[[CellState]]!
+    var grid:[[CellState]]!{
+        didSet{
+            self.setNeedsDisplay()
+        }
+    }
     
     func initCells() {
         cells = [[(rect:CGRect!, touchState:Bool)]](count: rows, repeatedValue:[(rect:CGRect!, touchState:Bool)](count:cols, repeatedValue:(nil, false)))
@@ -111,7 +115,8 @@ enum CellState:String{
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         for touch in touches{
-            self.manageTouchToggle(touch)
+//            self.manageTouchToggle(touch)
+            self.manageTouch(touch)
         }
     }
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -123,6 +128,9 @@ enum CellState:String{
 //        for touch in touches{
 //            self.manageTouchToggle(touch)
 //        }
+        for touch in touches{
+            self.manageTouchToggle(touch)
+        }
     }
     func manageTouchToggle(touch: UITouch){
         for i in 0..<rows{
@@ -155,4 +163,68 @@ enum CellState:String{
     }
 }
 
+func step2(arrayIn:[[Bool]]) -> [[Bool]] {
+    let ySize = arrayIn.count
+    let xSize = arrayIn[0].count
+    var after:[[Bool]] = []
+    for i in 0..<ySize{    //initialize after[] to be full of 'falses'
+        after.append([])
+        for _ in 0..<xSize{
+            after[i].append(false)
+        }
+    }
+    for i in 0..<ySize{    //step through 1 generation
+        for j in 0..<xSize{
+            var coords = (x:0, y:0) //'cause I don't want no enums
+            coords.y = i + ySize
+            coords.x = j + xSize
+            
+            var toCheck = neighbors(coords, sizeX: xSize, sizeY: ySize) //takes size constraints so can be used with arbitrarily sized arrays
+            var aliveNeighbors = 0
+            for k in 0..<8{ //counts number of living neighbors
+                if(arrayIn[toCheck[k].y][toCheck[k].x]) {
+                    aliveNeighbors += 1
+                }
+            }
+            coords.y = coords.y%ySize
+            coords.x = coords.x%xSize
+            
+            switch aliveNeighbors{
+            case 0,1:
+                after[coords.y][coords.x] = false
+            case 2:
+                after[coords.y][coords.x] = arrayIn[coords.y][coords.x]
+            case 3:
+                after[coords.y][coords.x] = true
+            case 4,5,6,7,8:
+                after[coords.y][coords.x] = false
+            default:
+                after[coords.y][coords.x] = false
+            }
+        }
+    }
+    return after
+}
 
+func neighbors(coordinates:(x:Int, y:Int), sizeX:Int, sizeY:Int) -> [(x:Int,y:Int)]{    //In case you're pedantic; takes size constraints so can be used with arbitrarily sized arrays
+    var around = simpleNeighbors(coordinates)
+    for i in 0..<8 {
+        around[i].x = around[i].x%sizeX  //doing fancy stuff with modulo - wrapping logic
+        around[i].y = around[i].y%sizeY
+    }
+    return around
+}
+
+
+func simpleNeighbors(coordinates:(x:Int, y:Int)) -> [(x:Int,y:Int)] { //You see, I had already made this function in problem 2 because it's a reasonable thing to do
+    var output:[(x:Int,y:Int)] = []
+    output.append((coordinates.x+1,coordinates.y))
+    output.append((coordinates.x+1,coordinates.y+1))
+    output.append((coordinates.x,coordinates.y+1))
+    output.append((coordinates.x-1,coordinates.y+1))
+    output.append((coordinates.x-1,coordinates.y))
+    output.append((coordinates.x-1,coordinates.y-1))
+    output.append((coordinates.x,coordinates.y-1))
+    output.append((coordinates.x+1,coordinates.y-1))
+    return output
+}
--- Assignment3/Assignment3/ViewController.swift
+++ Assignment3/Assignment3/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,7 +22,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stepButton(sender: AnyObject) {
+        var tempState:[[Bool]] = [[Bool]](count: lifeGrid.rows, repeatedValue:[Bool](count:lifeGrid.cols, repeatedValue: false))
+        for i in 0..<lifeGrid.rows{     //reading lifeGrid's grid array into a format step() can handle
+            for j in 0..<lifeGrid.cols{
+                switch lifeGrid.grid[i][j]{
+                case .Born,.Living:
+                    tempState[i][j] = true
+                default:
+                    tempState[i][j] = false
+                }
+            }
+        }
+        tempState = step2(tempState)
+        for i in 0..<lifeGrid.rows{     //reading back into lifeGrid's grid array
+            for j in 0..<lifeGrid.cols{
+                switch tempState[i][j]{
+                case true:
+                    lifeGrid.grid[i][j] = .Living
+                default:
+                    lifeGrid.grid[i][j] = .Empty
+                }
+            }
+        }
+        
+    }
 
+    @IBOutlet weak var lifeGrid: GridView!
 }
-
-
