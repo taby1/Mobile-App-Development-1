b477e10aa2dbc0e1e6365473649c1d90724c535f
taby1 <pyoon99@gmail.com>
2016-07-09 20:44:48 -0400

    Cell Drawing
    Now it works properly

--- Assignment3/Assignment3/Engine.swift
+++ Assignment3/Assignment3/Engine.swift
@@ -33,12 +33,16 @@ enum CellState:String{
 @IBDesignable class GridView: UIView {
     @IBInspectable var rows:Int = 20{
         didSet{
+            initCells()
             grid = [[CellState]](count: rows, repeatedValue:[CellState](count: cols, repeatedValue: .Empty))
+            self.setNeedsDisplay()
         }
     }
     @IBInspectable var cols:Int = 20{
         didSet{
+            initCells()
             grid = [[CellState]](count: rows, repeatedValue:[CellState](count: cols, repeatedValue: .Empty))
+            self.setNeedsDisplay()
         }
     }
     @IBInspectable var livingColor:UIColor = UIColor.blueColor()
@@ -46,8 +50,26 @@ enum CellState:String{
     @IBInspectable var bornColor:UIColor = UIColor.greenColor()
     @IBInspectable var diedColor:UIColor = UIColor.redColor()
     @IBInspectable var gridColor:UIColor = UIColor.blackColor()
-    @IBInspectable var gridWidth:CGFloat = CGFloat(1.0)
+    @IBInspectable var gridWidth:CGFloat = CGFloat(1.0){
+        didSet{
+            initCells()
+        }
+    }
     var grid:[[CellState]]!
+    
+    func initCells() {
+        cells = [[CGRect!]](count: rows, repeatedValue:[CGRect!](count:cols, repeatedValue:nil))
+        for i in 0..<rows{
+            for j in 0..<cols{
+                let width = (bounds.width / CGFloat(cols)) - (gridWidth + (gridWidth / CGFloat(cols)))
+                let height = (bounds.height / CGFloat(rows)) - (1 * gridWidth)
+                let x = CGFloat(j) * ((bounds.width - (CGFloat(cols + 1) * gridWidth)) / CGFloat(cols)) + (gridWidth * (CGFloat(j) + 1))   //Oh god jesus christ don't ask me about this
+                let y = CGFloat(i) * ((bounds.height - (CGFloat(rows + 1) * gridWidth)) / CGFloat(rows)) + (gridWidth * (CGFloat(i) + 1))   //Oh god jesus christ don't ask me about this
+                cells[i][j] = CGRect(x: x, y: y, width: width, height: height)
+            }
+        }
+    }
+    var cells:[[CGRect!]] = []/* = [[CGRect!]](count: rows, repeatedValue:[CGRect!](count:cols, repeatedValue:nil))*/  //I get a funny feeling like this'll be useful later for touch detection as it is my understanding that checking for touches in individual rectanges is easy and this way I can just iterate through an array checking for touches
     
     override func drawRect(rect: CGRect) {
         
@@ -65,6 +87,17 @@ enum CellState:String{
         }
         gridColor.setStroke()
         gridPath.stroke()
+        
+        
+        let cellPaths = UIBezierPath()
+        cellPaths.lineWidth = CGFloat(0)
+        for i in 0..<rows{
+            for j in 0..<cols{
+                cellPaths.appendPath(UIBezierPath(ovalInRect: cells[i][j]))
+            }
+        }
+        emptyColor.setFill()
+        cellPaths.fill()
     }
 }
 
