11467304210b03090950e03973f1aa5d717beb11
taby1 <pyoon99@gmail.com>
2016-07-14 17:17:19 -0400

    Started Assignment 4
    Started

---
+++ Assignment4/Assignment4/Protocols.swift
@@ -0,0 +1,14 @@
+//
+//  Protocols.swift
+//  Assignment4
+//
+//  Created by Patrick on 7/14/16.
+//  Copyright © 2016 Taby Corp. All rights reserved.
+//
+
+import Foundation
+
+
+protocol GridProtocol{
+    
+}
