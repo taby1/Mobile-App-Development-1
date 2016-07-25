29e90e92e7be2b7537ada53fb0f2ba24d99f6374
taby1 <pyoon99@gmail.com>
2016-07-25 16:50:34 -0400

    asdf
    midlecture

---
+++ Lectrue 10/Lectrue 10/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Lectrue 10
+//
+//  Created by Patrick on 7/25/16.
+//  Copyright © 2016 Taby Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	@IBOutlet weak var textView: UITextView!
+	@IBAction func fetch(sender: AnyObject) {
+		let url = NSURL(string: "http://www.google.com/")!
+		let fetcher = Fetcher()
+		fetcher.requestJSON(url) { (json, message) in
+			if let json = json{
+				self.textView.text = json.description
+			}
+			else if let message = message{
+				self.textView.text = message
+			}
+			else{
+				self.textView.text = "SLDKJF"
+			}
+		}
+	}
+
+}
+
